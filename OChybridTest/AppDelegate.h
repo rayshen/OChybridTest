5bd4812e264201725b8d884b9fe90d10490d9178
rayshen <rayshen1012@gmail.com>
2015-11-19 13:11:07 +0800

    Initial Commit

---
+++ OChybridTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OChybridTest
+//
+//  Created by shen on 15/11/19.
+//  Copyright © 2015年 shen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OChybridTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OChybridTest
+//
+//  Created by shen on 15/11/19.
+//  Copyright © 2015年 shen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
